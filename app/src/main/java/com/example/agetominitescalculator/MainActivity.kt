6a6b08c8c4c91d32715a8c1e807b17ad3047fb7c
MSI\harsh <hdp21091995@gmail.com>
2022-04-12 16:13:07 -0400

    Adding counts of Days since user's birthdate which will be inserted via user using DatePickerDialog

--- app/src/main/java/com/example/agetominitescalculator/MainActivity.kt
+++ app/src/main/java/com/example/agetominitescalculator/MainActivity.kt
@@ -2,61 +2,71 @@ package com.example.agetominitescalculator
 
 import android.app.DatePickerDialog
 import android.icu.text.SimpleDateFormat
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import java.time.Year
+import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var datePickerButton:Button
+    private lateinit var datePickerButton: Button
     private lateinit var selectedDateTextView: TextView
-    private lateinit var ageInMinitesTextView:TextView
+    private lateinit var ageInMinitesTextView: TextView
+    private lateinit var ageInDays: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        datePickerButton=findViewById(R.id.btnDatePicker)
-        selectedDateTextView=findViewById(R.id.tvSelectedDate)
-        ageInMinitesTextView=findViewById(R.id.tvAgeInMinetes)
+        datePickerButton = findViewById(R.id.btnDatePicker)
+        selectedDateTextView = findViewById(R.id.tvSelectedDate)
+        ageInMinitesTextView = findViewById(R.id.tvAgeInMinetes)
+        ageInDays = findViewById(R.id.tvAgeInDays)
 
         datePickerButton.setOnClickListener {
             clickDatePicker()
         }
     }
 
-    private fun clickDatePicker(){
+    private fun clickDatePicker() {
         val myCalendar = Calendar.getInstance()
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
-        with(DatePickerDialog(this,
-            { _, selecetdYear, selectedMonth, selectedDay ->
-                Toast.makeText(this,"Year $selecetdYear, Month ${selectedMonth + 1}, Day $selectedDay",Toast.LENGTH_SHORT).show()
+        with(
+            DatePickerDialog(
+                this,
+                { _, selecetdYear, selectedMonth, selectedDay ->
+                    Toast.makeText(
+                        this,
+                        "Year $selecetdYear, Month ${selectedMonth + 1}, Day $selectedDay",
+                        Toast.LENGTH_SHORT
+                    ).show()
 
-                val selectedDate="$selectedDay/${selectedMonth+1}/$selecetdYear"
+                    val selectedDate = "$selectedDay/${selectedMonth + 1}/$selecetdYear"
 
-                selectedDateTextView.text = selectedDate
+                    selectedDateTextView.text = selectedDate
 
+                    val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.CANADA)
+                    val theDate = sdf.parse("$selectedDay/${selectedMonth + 1}/$selecetdYear")
 
-                val sdf=SimpleDateFormat("dd/MM/yyyy", Locale.CANADA)
+                    val selecetedDatesInMinutes = theDate.time / 60000
+                    val currentDatesInMinutes =
+                        sdf.parse(sdf.format(System.currentTimeMillis())).time / 60000
 
-                val theDate=sdf.parse("$selectedDay/${selectedMonth+1}/$selecetdYear")
+                    ageInMinitesTextView.text =
+                        (currentDatesInMinutes - selecetedDatesInMinutes).toString()
 
-                val selecetedDatesInMinutes=theDate.time /60000
+                    val selectedDateInDays = theDate.time / 86400000
+                    val currentDateinDays =
+                        sdf.parse(sdf.format(System.currentTimeMillis())).time / 86400000
 
-                val currentDatesInMinutes=sdf.parse(sdf.format(System.currentTimeMillis())).time /60000
-
-                ageInMinitesTextView.text = (currentDatesInMinutes - selecetedDatesInMinutes).toString()
-
-            }
-            ,year
-            ,month
-            ,day)){
+                    ageInDays.text = (currentDateinDays - selectedDateInDays).toString()
+                }, year, month, day
+            )
+        ) {
             datePicker.maxDate = System.currentTimeMillis() - 86400000
             show()
         }
